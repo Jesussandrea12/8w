2fc623954854757c602871b26b4be836bc02f33e
jesus sandrea <jesussandrea12@gmail.com>
2018-03-28 00:08:05 -0400

    css minificado

--- gulpfile.js
+++ gulpfile.js
@@ -32,7 +32,7 @@ gulp.task('css', function () {
     csswring()
   ]
 
-  return gulp.src('./src/*.css')
+  return gulp.src('./src/main.css')
     .pipe(postcss(processors))
     .pipe(gulp.dest('./dist/css'))
     .pipe(browserSync.stream())
