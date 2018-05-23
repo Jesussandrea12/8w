d5816db08776cd3f89f3fda8300bf30c4e65e5b4
jesus sandrea <jesussandrea12@gmail.com>
2018-05-23 08:34:18 -0400

    overlay images added

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,23 @@
-$(document).foundation()
+$(document).foundation();
+
+// open overlays
+$('#8w').on('click', function() {
+  $('.overlay-one').addClass('is-active')
+});
+$('#ko').on('click', function() {
+  $('.overlay-two').addClass('is-active')
+});
+$('#banner-btn').on('click', function() {
+  $('.overlay-three').addClass('is-active')
+});
+
+// close overlays
+$('.close-overlay-one').on('click', function() {
+  $('.overlay-one').removeClass('is-active')
+});
+$('.close-overlay-two').on('click', function() {
+  $('.overlay-two').removeClass('is-active')
+});
+$('.close-overlay-three').on('click', function() {
+  $('.overlay-three').removeClass('is-active')
+});
